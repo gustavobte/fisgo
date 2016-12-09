041c8108541b0e4563732b7ed69850d4afecf253
Ferran Gonzalez Alonso <gaferran@gmail.com>
2016-12-09 15:42:27 -0200

    #8 - Commit inicial

---
+++ www/js/app.js
@@ -0,0 +1,16 @@
+angular.module('fisgo', ['ionic'])
+
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+        // Nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
