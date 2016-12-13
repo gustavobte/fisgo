3ae3272fb22ce79234ee2039f4b5febc15d4862e
Ferran Gonzalez Alonso <gaferran@gmail.com>
2016-12-13 17:45:31 -0200

    #4 - Removidas linhas desnecessárias do leitor.html

--- www/modules/leitor/leitor.controller.js
+++ www/modules/leitor/leitor.controller.js
@@ -11,27 +11,22 @@
         var vm = this;
 
         vm.resultadoLeitor = ''
+        vm.error = 'sem erro';
 
         vm.leitorBarcode = function () {
             cordova.plugins.barcodeScanner.scan(
                 function (result) {
-                    vm.resultadoLeitor = result;
-                    console("We got a barcode\n" +
-                        "Result: " + result.text + "\n" +
-                        "Format: " + result.format + "\n" +
-                        "Cancelled: " + result.cancelled);
+                    vm.resultadoLeitor = result.text;
                 },
                 function (error) {
                     console("Scanning failed: " + error);
                 },
                 {
-                    "preferFrontCamera": true, // iOS and Android
-                    "showFlipCameraButton": true, // iOS and Android
-                    "showTorchButton": true, // iOS and Android
-                    "disableAnimations": true, // iOS
+                    "preferFrontCamera": true,
+                    "showFlipCameraButton": true,
+                    "showTorchButton": true,
                     "prompt": "Por favor, centralize o código de barras", // supported on Android only
-                    "formats": "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-                    "orientation": "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
+                    "formats": "QR_CODE,PDF_417" // default: all but PDF_417 and RSS_EXPANDED
                 }
             );
         };
